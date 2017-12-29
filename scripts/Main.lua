dcd2d8252bdca44185e965f940eb4acf76dbb808
MininMobile <miningamer@gmail.com>
2017-12-29 21:38:13 +0000

    Hide on Launch
    And add Comments

--- scripts/Main.lua
+++ scripts/Main.lua
@@ -1,8 +1,11 @@
+-- Get keys from config file
 local Config = require "Config"
 local Keys = Config.config;
 
+-- Assign numpad to it's ID
 lmc_device_set_name("numpad", "884B96C3");
 
+-- Sent to AutoHotKey Function
 sendToAHK = function (key)
     local file = io.open("F:\\Data\\Documents\\Programs\\luamacros\v2\\scripts\\keypressed.txt", "w")
 
@@ -14,6 +17,7 @@ sendToAHK = function (key)
     lmc_send_keys('{F24}')
 end
 
+-- Event Handler
 lmc_set_handler("numpad", function(button, direction)
     if (direction == 1) then return end
 
@@ -24,4 +28,9 @@ lmc_set_handler("numpad", function(button, direction)
     end
 end)
 
+-- Tell when Launched
 print("Launched")
+
+-- Hide
+lmc.minimizeToTray = true
+lmc_minimize()
